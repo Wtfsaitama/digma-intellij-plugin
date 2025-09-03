00e2df479a6f61e9da7c205ec625a67962ef236d
Wtfsaitama <59986319+Wtfsaitama@users.noreply.github.com>
2025-09-03 23:09:22 +0200

    Revert "Merge pull request #2778 from digma-ai/update-build-profiles"
    This reverts commit 7af538afcebee86cf0a69d7b3bf1bda005232864, reversing
    changes made to c950d0d6456e7df22e21430695ab8ab3d26961d9.

--- common-build-logic/src/main/kotlin/common/rider/RiderCommon.kt
+++ common-build-logic/src/main/kotlin/common/rider/RiderCommon.kt
@@ -7,8 +7,8 @@ fun rdGenVersion(profile: String): String {
 
     val profileToUse: BuildProfiles.Profile = when (profile) {
         "lowest" -> BuildProfiles.Profile.p241
-        "latest" -> BuildProfiles.Profile.p252
-        "eap" -> BuildProfiles.Profile.p253
+        "latest" -> BuildProfiles.Profile.p251
+        "eap" -> BuildProfiles.Profile.p252
         else -> BuildProfiles.Profile.valueOf(profile)
     }
 
@@ -24,6 +24,5 @@ fun rdGenVersionByProfile(profile: BuildProfiles.Profile): String {
         BuildProfiles.Profile.p243 -> "2024.3.1"
         BuildProfiles.Profile.p251 -> "2025.1.1"
         BuildProfiles.Profile.p252 -> "2025.1.1"
-        BuildProfiles.Profile.p253 -> "2025.1.1"
     }
 }
